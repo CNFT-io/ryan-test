1af400f8cd1e6edf9c76e767649c851263565dea
Professor Cardano <r@cnft.io>
2021-10-25 16:03:27 +0100

    More cool stuff

--- ryanapi/api/__init__.py
+++ ryanapi/api/__init__.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def hello_world():
     return "<p>Hello, World!</p>"
 
+@app.route("/goodbye")
+def goodbye_world():
+    return "<p>Goodbye, World!</p>"
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
